68846f771846402e9a14874572fdc8b05032807f
Flipez <code@brauser.io>
2019-12-04 19:56:27 +0100

    day4
    Signed-off-by: Flipez <code@brauser.io>

---
+++ day4/part_1.rb
@@ -0,0 +1,20 @@
+input = 284639..748759
+matching_numbers = 0
+
+input.each do |number|
+  digits = number.to_s.chars.map(&:to_i)
+  is_larger = []
+  digits.each_with_index{|d,i| is_larger << (d >= digits[i-1])}
+  is_larger = is_larger[1..-1]
+
+  next unless is_larger.all?
+
+  %w(11 22 33 44 55 66 77 88 99).each do |double|
+    if number.to_s.include? double
+      matching_numbers += 1
+      break
+    end
+  end
+end
+
+p matching_numbers
