25782d337b8e8e3dc4c2a443627b7c128e043257
Flipez <code@brauser.io>
2019-12-04 19:18:59 +0100

    day3
    Signed-off-by: Flipez <code@brauser.io>

---
+++ day3/part_1.rb
@@ -0,0 +1,52 @@
+wires = File.read('input').split("\n")
+
+wire_1 = [[0,0]]
+wire_2 = [[0,0]]
+
+wires[0].split(',').each do |instruction|
+  direction = instruction[0]
+  lenght = instruction[1..-1].to_i
+  x = wire_1[-1][0]
+  y = wire_1[-1][1]
+  case direction
+  when 'R'
+    lenght.times { |i| wire_1 << [x + (i + 1), y]}
+  when 'L'
+    lenght.times { |i| wire_1 << [x - (i + 1), y]}
+  when 'U'
+    lenght.times { |i| wire_1 << [x, y + (i + 1)]}
+  when 'D'
+    lenght.times { |i| wire_1 << [x, y - (i + 1)]}
+  end
+end
+
+wires[1].split(',').each do |instruction|
+  direction = instruction[0]
+  lenght = instruction[1..-1].to_i
+  x = wire_2[-1][0]
+  y = wire_2[-1][1]
+  case direction
+  when 'R'
+    lenght.times { |i| wire_2 << [x + (i + 1), y]}
+  when 'L'
+    lenght.times { |i| wire_2 << [x - (i + 1), y]}
+  when 'U'
+    lenght.times { |i| wire_2 << [x, y + (i + 1)]}
+  when 'D'
+    lenght.times { |i| wire_2 << [x, y - (i + 1)]}
+  end
+end
+
+wire_1.delete([0,0])
+wire_2.delete([0,0])
+
+intersections = (wire_1 & wire_2)
+steps = []
+
+intersections.each do |intersection|
+  a = wire_1.index(intersection) + 1
+  b = wire_2.index(intersection) + 1
+  steps << a + b
+end
+
+p steps.sort.first
