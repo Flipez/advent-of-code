36feffd47f30ecd457130c4718f8f4137eab1f09
Flipez <code@brauser.io>
2019-12-11 23:35:38 +0100

    partly refactor
    Signed-off-by: Flipez <code@brauser.io>

--- day7/part_1.cr
+++ day7/part_1.cr
@@ -1,127 +1,134 @@
 original_intcodes = File.read("input").split(",")
 outputs = [] of Int32
-original_inputs = [0,1,2,3,4].permutations.to_a
+original_inputs = [0, 1, 2, 3, 4].permutations.to_a
 
-def parse_parameter_value(position, mode, intcodes)
-  case mode
-  when 0
-    value_position = intcodes[position.to_i]
-    intcodes[value_position.to_i]
-  when 1
-    intcodes[position.to_i]
+class IntcodeComputer
+  getter :intcode, :outputs
+  property :position, :done
+
+  def initialize(intcode : String)
+    @position = 0
+    @done = false
+    @intcode = [] of Int32
+    @inputs = [] of Int32
+    @outputs = [] of Int32
+
+    parse_intcode(intcode)
+
+    until done
+      process_current_position
+    end
   end
-end
 
-def calculate_output(intcodes, inputs, start_value, iteration, outputs)
-  original_intcodes = intcodes
-  current_position = 0
-  current_opcode = 0
-  input_count = 0
+  def add_input(input : Int32)
+    @inputs.push(input)
+  end
 
-  until current_opcode == 99
-    instructions = "%05d" % intcodes[current_position].to_i
-    current_opcode = instructions[-2..-1].to_i
-    first_parameter_mode = instructions[2].to_i
-    second_parameter_mode = instructions[1].to_i
-    third_parameter_mode = instructions[0].to_i
+  def next_input
+    @inputs.shift
+  end
 
-    case current_opcode
+  def add_output(output : Int32)
+    @outputs.push(output)
+  end
+
+  def next_output
+    @outputs.shift
+  end
+
+  private def parse_intcode(intcode)
+    @intcode = intcode.split(",").map do |code|
+      code.to_i
+    end
+  end
+
+  private def move_position(offset : Int32)
+    @position += offset
+  end
+
+  def process_current_position
+    instr = "%05d" % intcode[position]
+    opcode = instr[-2..-1].to_i
+
+    first_param_mode = instr[2].to_i
+    second_param_mode = instr[1].to_i
+    third_param_mode = instr[0].to_i
+
+    case opcode
     when 1
-      a = parse_parameter_value(current_position + 1, first_parameter_mode, intcodes)
-      b = parse_parameter_value(current_position + 2, second_parameter_mode, intcodes)
-      target = intcodes[current_position + 3]
+      a, b = param(1, first_param_mode), param(2, second_param_mode)
+      target = intcode[position + 3]
 
-      if a && b && target
-        intcodes[target.to_i] = (a.to_i + b.to_i).to_s
-        current_position += 4
-      end
+      intcode[target] = (a + b)
+      move_position(4)
     when 2
-      a = parse_parameter_value(current_position + 1, first_parameter_mode, intcodes)
-      b = parse_parameter_value(current_position + 2, second_parameter_mode, intcodes)
-      target = intcodes[current_position + 3].to_i
+      a, b = param(1, first_param_mode), param(2, second_param_mode)
+      target = intcode[position + 3]
 
-      if a && b && target
-        intcodes[target.to_i] = (a.to_i * b.to_i).to_s
-        current_position += 4
-      end
+      intcode[target] = (a * b)
+      move_position(4)
     when 3
-      target = intcodes[current_position + 1]
+      target = intcode[position + 1]
 
-      if target
-        if input_count == 0
-          intcodes[target.to_i] = inputs[iteration].to_s
-        elsif input_count == 1
-          intcodes[target.to_i] = start_value.to_s
-        end
-        input_count += 1
-        current_position += 2
-      end
+      intcode[target] = next_input
+      move_position(2)
     when 4
-      next_start_value = parse_parameter_value(current_position + 1, first_parameter_mode, intcodes) 
-      if iteration == 4 && next_start_value
-        outputs << next_start_value.to_i
-        break
-      else
-        calculate_output(original_intcodes, inputs, next_start_value, iteration + 1, outputs) unless iteration == 4
-        current_position += 2
-      end
-    when 5
-      a = parse_parameter_value(current_position + 1, first_parameter_mode, intcodes)
-      b = parse_parameter_value(current_position + 2, second_parameter_mode, intcodes)
+      output = param(1, first_param_mode)
 
-      if a && b
-        if a.to_i.zero?
-          current_position += 3
-        else
-          current_position = b.to_i
-        end
+      add_output(output)
+      move_position(2)
+    when 5
+      a, b = param(1, first_param_mode), param(2, second_param_mode)
+
+      if a.zero?
+        move_position(3)
+      else
+        @position = b
       end
     when 6
-      a = parse_parameter_value(current_position + 1, first_parameter_mode, intcodes)
-      b = parse_parameter_value(current_position + 2, second_parameter_mode, intcodes)
+      a, b = param(1, first_param_mode), param(2, second_param_mode)
 
-      if a && b
-        if a.to_i.zero?
-          current_position = b.to_i
-        else
-          current_position += 3
-        end
+      if a.zero?
+        @position = b
+      else
+        move_position(3)
       end
     when 7
-      a = parse_parameter_value(current_position + 1, first_parameter_mode, intcodes)
-      b = parse_parameter_value(current_position + 2, second_parameter_mode, intcodes)
-      c = intcodes[current_position + 3]
+      a, b = param(1, first_param_mode), param(2, second_param_mode)
+      target = intcode[position + 3]
 
-      if a && b && c
-        if a.to_i < b.to_i
-          intcodes[c.to_i] = 1.to_s
-        else
-          intcodes[c.to_i] = 0.to_s
-        end
-        current_position += 4
+      if a < b
+        intcode[target] = 1
+      else
+        intcode[target] = 0
       end
+      move_position(4)
     when 8
-      a = parse_parameter_value(current_position + 1, first_parameter_mode, intcodes)
-      b = parse_parameter_value(current_position + 2, second_parameter_mode, intcodes)
-      c = intcodes[current_position + 3]
+      a, b = param(1, first_param_mode), param(2, second_param_mode)
+      target = intcode[position + 3]
 
-      if a && b && c
-        if a.to_i == b.to_i
-          intcodes[c.to_i] = 1.to_s
-        else
-          intcodes[c.to_i] = 0.to_s
-        end
-        current_position += 4
+      if a == b
+        intcode[target] = 1
+      else
+        intcode[target] = 0
       end
+      move_position(4)
     when 99
-      break
+      @done = true
+    end
+  end
+
+  private def param(offset : Int32, mode : Int32)
+    case mode
+    when 0
+      value_position = intcode[position + offset]
+      intcode[value_position]
+    when 1
+      intcode[position + offset]
+    else
+      raise Exception.new("Invalid mode")
     end
   end
 end
 
-original_inputs.each do |inputs|
-  # intcodes, inputs, start_Value, iteration
-  output = calculate_output(original_intcodes, inputs, 0, 0, outputs)
-end
-
-p outputs.sort.last
+IntcodeComputer.new(File.read("input"))
